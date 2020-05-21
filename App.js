2da08ad990d6b467861bb3aafedaf8ac42465a11
Alexander Belashej <a.belashej@tapston.com>
2020-05-21 13:33:01 +0300

    extracted main square into separate component

--- App.js
+++ App.js
@@ -1,85 +1,61 @@
 import React, { Component } from "react";
-import { Animated, StyleSheet, View, TouchableOpacity } from "react-native";
-import { PanGestureHandler, State } from "react-native-gesture-handler";
+import {
+  Animated,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 
-const inputRange = [-101, -100, 0, 100, 101];
+import ListItem from "./src/ListItem";
 
 export default class DraggableBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isInDeleteState: false,
+      isCollided: false,
+      isInSwipeMode: false,
+      isInCollisionMode: false,
     };
     this._translateY = new Animated.Value(0);
     this._animatedDelete = new Animated.Value(1);
     this._lastOffset = { y: 0 };
-
-    this._onGestureEventY = Animated.event(
-      [
-        {
-          nativeEvent: {
-            translationY: this._translateY,
-          },
-        },
-      ],
-      { useNativeDriver: true }
-    );
   }
 
-  _onHandlerStateChangeY = (event) => {
-    if (event.nativeEvent.oldState === State.ACTIVE) {
-      this._lastOffset.y += event.nativeEvent.translationY;
-      this._translateY.setOffset(this._lastOffset.y);
-      this._translateY.setValue(0);
-    }
+  setIsInSwipeMode = (isInSwipeMode) => {
+    this.setState({ isInSwipeMode });
   };
 
-  onDelete = () => {
-    this.setState({ isInDeleteState: true }, () => {
-      Animated.timing(this._animatedDelete, {
-        toValue: 0,
-        duration: 2000,
-      }).start();
-    });
+  setIsInCollisionMode = (isInCollisionMode) => {
+    this.setState({ isInCollisionMode });
   };
 
   render() {
     return (
-      <View style={styles.scrollView}>
-        <PanGestureHandler
-          {...this.props}
-          onGestureEvent={this._onGestureEventY}
-          onHandlerStateChange={this._onHandlerStateChangeY}
-        >
-          <Animated.View
-            style={[
-              styles.mainSquare,
-              {
-                transform: [{ translateY: this._translateY }],
-              },
-              this.state.isInDeleteState && {
-                transform: [
-                  {
-                    translateY: this._animatedDelete.interpolate({
-                      inputRange: [0, 1],
-                      outputRange: [-200, 0],
-                    }),
-                  },
-                ],
-              },
-              { opacity: this._animatedDelete },
-            ]}
-          />
-        </PanGestureHandler>
+      <View style={styles.container}>
+        <FlatList
+          style={styles.flatList}
+          horizontal
+          data={[1, 2, 3, 4]}
+          keyExtractor={(item) => item.toString()}
+          renderItem={({ item }) => (
+            <ListItem
+              setIsInSwipeMode={this.setIsInSwipeMode}
+              setIsInCollisionMode={this.setIsInCollisionMode}
+              isLast={item === 4}
+              isInCollisionMode={this.state.isInCollisionMode}
+            />
+          )}
+        />
         <Animated.View
           style={[
             styles.smallSquare,
             styles.top,
-            {
-              opacity: this._translateY.interpolate({
-                inputRange,
-                outputRange: [1, 0, 0, 0, 1],
-              }),
+            { opacity: this.state.isInSwipeMode },
+            this.state.isInCollisionMode && {
+              transform: [{ scale: 1.2 }],
             },
           ]}
         />
@@ -87,23 +63,16 @@ export default class DraggableBox extends Component {
           style={[
             styles.smallSquare,
             styles.bottom,
-            {
-              opacity: this._translateY.interpolate({
-                inputRange,
-                outputRange: [1, 0, 0, 0, 1],
-              }),
+            { opacity: this.state.isInSwipeMode },
+            this.state.isInCollisionMode && {
+              transform: [{ scale: 1.2 }],
             },
           ]}
         />
         <Animated.View
           style={[
             styles.bottomContainer,
-            {
-              opacity: this._translateY.interpolate({
-                inputRange,
-                outputRange: [0, 0, 1, 0, 0],
-              }),
-            },
+            { opacity: !this.state.isInSwipeMode },
           ]}
         >
           <TouchableOpacity onPress={this.onDelete} style={styles.button} />
@@ -114,17 +83,13 @@ export default class DraggableBox extends Component {
 }
 
 const styles = StyleSheet.create({
-  scrollView: {
+  container: {
     flex: 1,
     justifyContent: "center",
   },
-  mainSquare: {
-    width: 150,
-    height: 150,
-    position: "absolute",
-    alignSelf: "center",
-    backgroundColor: "yellow",
-    margin: 10,
+  flatList: {
+    flex: 1,
+    paddingLeft: 75,
   },
   smallSquare: {
     position: "absolute",
@@ -132,13 +97,12 @@ const styles = StyleSheet.create({
     height: 50,
     alignSelf: "center",
     backgroundColor: "deepskyblue",
-    margin: 10,
   },
   top: {
-    top: 10,
+    top: 20,
   },
   bottom: {
-    bottom: 10,
+    bottom: 20,
   },
   bottomContainer: {
     position: "absolute",
---
+++ src/ListItem.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import { Animated, StyleSheet, Dimensions } from "react-native";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+
+const { height } = Dimensions.get("window");
+
+export default class ListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isInDeleteState: false,
+      isCollided: false,
+    };
+    this._translateY = new Animated.Value(0);
+    this._animatedDelete = new Animated.Value(1);
+    this._lastOffset = { y: 0 };
+  }
+
+  _onGestureEventY = ({ nativeEvent }) => {
+    this._translateY.setValue(nativeEvent.translationY);
+    this.props.setIsInSwipeMode(Math.abs(nativeEvent.translationY) > 100);
+    this.props.setIsInCollisionMode(
+      Math.abs(nativeEvent.translationY) >= height / 2 - 170
+    );
+    this.setState({
+      isCollided: Math.abs(nativeEvent.translationY) >= height / 2 - 170,
+    });
+  };
+
+  _onHandlerStateChangeY = (event) => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      this._lastOffset.y += event.nativeEvent.translationY;
+      this._translateY.setOffset(this._lastOffset.y);
+      this._translateY.setValue(0);
+      if (Math.abs(this._lastOffset.y) <= height / 2 - 70 - 100) {
+        Animated.timing(this._translateY, {
+          toValue: this._translateY.flattenOffset(),
+          duration: 500,
+          useNativeDriver: true,
+        }).start(() => {
+          this._lastOffset.y = 0;
+          this._translateY.setOffset(0);
+          this._translateY.setValue(0);
+          this.props.setIsInSwipeMode(false);
+        });
+      } else {
+      }
+    }
+  };
+
+  onDelete = () => {
+    this.setState({ isInDeleteState: true }, () => {
+      Animated.timing(this._animatedDelete, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: true,
+      }).start(() => {
+        this.setState({ isInDeleteState: false }, () =>
+          this._animatedDelete.setValue(1)
+        );
+      });
+    });
+  };
+
+  render() {
+    return (
+      <PanGestureHandler
+        onGestureEvent={this._onGestureEventY}
+        onHandlerStateChange={this._onHandlerStateChangeY}
+      >
+        <Animated.View
+          style={[
+            styles.mainSquare,
+            {
+              transform: [
+                { translateY: this._translateY },
+                { scale: this.state.isCollided ? 0.8 : 1 },
+              ],
+            },
+            this.state.isInDeleteState && {
+              transform: [
+                {
+                  translateY: this._animatedDelete.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [-200, 0],
+                  }),
+                },
+              ],
+            },
+            { opacity: this._animatedDelete },
+            this.props.isLast && { marginRight: 170 },
+          ]}
+        />
+      </PanGestureHandler>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainSquare: {
+    width: 200,
+    height: 200,
+    margin: 10,
+    alignSelf: "center",
+    backgroundColor: "yellow",
+  },
+});
